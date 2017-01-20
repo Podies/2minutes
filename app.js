a817dd79ddeb756532bfc10152549a9da4e31037
Sachin Kumaar <sachinkumaar91@gmail.com>
2017-01-20 16:12:14 +0530

    changes

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var compiler = webpack(config);
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-var User = require('./models/user');
 const MongoStore = require('connect-mongo')(session);
 mongoose.Promise = global.Promise;
 
@@ -60,20 +59,21 @@ app.use(passport.session());
 var passportService = require('./auth/passport')(passport);
 
 // Route handler
-app.use('/auth', routes);
+app.use('/', routes);
 app.use('/users', users);
 app.use('/api', api);
 
 app.use('*', function(req, res) {
-  var initialState = { activeUser: null };
-  if(req.user) {
-    User.findById(req.user._id).select('-password').exec(function(err, user){
-      initialState.activeUser = user;
-      res.render('index', { title: '2 minutes', initialState });
-    });
-  } else {
-    res.render('index', { title: '2 minutes', initialState });
-  }
+  res.render('index');
+});
+
+
+app.use('*', function(req, res) {
+  res.render('index');
+});
+
+app.use('*', function(req, res) {
+  res.render('index');
 });
 
 /// catch 404 and forwarding to error handler
