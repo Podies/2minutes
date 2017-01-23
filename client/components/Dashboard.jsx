d9965af844d3588eda43218f54f7eef2b98b7f2f
Sachin Kumaar <sachinkumaar91@gmail.com>
2017-01-23 19:14:08 +0530

    Style Dashboard/Questionnaire Page

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -10,18 +10,6 @@ const Dashboard = () => {
     	<div className="grid">
     	  <div className="row">
 	    	  <div className="col-xs-12 dashboard-wrapper">
-		    	  <div className="col-xs-12 nav-menu">
-		    	    <div className="col-xs-4">
-			  	       <div className="col">
-				  	        <ul className="nav-tabs">
-				  	          <li><a href="#" className="my-questions">My Questions</a></li>
-				  	          <li><a href="#" className="my-stats">My Stats</a></li>
-				  	        </ul>
-			  	       </div>
-		    	    </div>
-		    	    <div className="col-xs-8">
-		    	    </div>
-		    	  </div>
 		    	  <Questions />
             {/*<Calendar />*/}
 	    	  </div>
--- client/components/UserDropDown.jsx
+++ client/components/UserDropDown.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const UserDropDown = () => {
   return (
-    <ul>
-      <li>User Profile</li>
-      <li>Stats</li>
-      <li>Learn</li>
+    <ul className="user-profile-menu">
+      <li><a  href=""><i className="fa fa-user-circle-o" aria-hidden="true"></i>Profile</a></li>
+      <li><a  href=""><i className="fa fa-bar-chart" aria-hidden="true"></i>Stats</a></li>
+      <li><a href=""><i className="fa fa-list-ul" aria-hidden="true"></i>Questions</a></li>
+      <li><a href=""><i className="fa fa-list-ul" aria-hidden="true"></i>Log Out</a></li>
     </ul>
   );
 }
