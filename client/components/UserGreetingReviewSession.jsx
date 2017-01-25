2b64ff1d5b82ae1759e487e8b7fe5cfee4672dff
Sachin Kumaar <sachinkumaar91@gmail.com>
2017-01-25 14:34:39 +0530

    Initial style feedback session, add multiple components for feedback session

---
+++ client/components/UserGreetingReviewSession.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const UserGreetingReviewSession = () => {
+	return (
+    <div>
+       <div className="col">
+          <h1 className="feedback-greeting">Hey! Howdy, let's begin your feedback session ☺</h1>
+       </div>
+    </div>
+	)
+}
+
+export default UserGreetingReviewSession;
